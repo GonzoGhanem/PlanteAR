33344bc23b988b102ff16fff37a0f40927cea47a
GonzoGhanem <ghanemgo@gmail.com>
2013-04-17 20:01:30 -0300

    Fixed showing more errors than the expected.

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -9,13 +9,13 @@ class LineItem < ActiveRecord::Base
   accepts_nested_attributes_for :product
 
   validates_presence_of :amount, :message => "La cantidad no puede estar en blanco"
-  validates_numericality_of :amount, :greater_than => 0, :message => "La cantidad no puede ser Negativa"
+  validates_numericality_of :amount, :greater_than => 0, :unless => lambda { self.amount.blank? },:message => "La cantidad no puede ser Negativa"
 
   validates_presence_of :unit_price, :message => "El precio unitario no puede estar en blanco"
-  validates_numericality_of :unit_price, :greater_than => 0, :message => "El precio unitario no puede ser Negativo"
+  validates_numericality_of :unit_price, :greater_than => 0, :unless => lambda { self.unit_price.blank? }, :message => "El precio unitario no puede ser Negativo"
   
   validates_presence_of :subtotal, :message => "El subtotal no puede estar en blanco"
-  validates_numericality_of :amount, :greater_than => 0, :message => "El subtotal no puede ser negativo"
+  validates_numericality_of :subtotal, :greater_than => 0,:unless => lambda { self.subtotal.blank? }, :message => "El subtotal no puede ser negativo"
   
 
 end
