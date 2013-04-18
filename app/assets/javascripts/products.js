90d0492211f053b0977c18cb797fc207eaa9ac12
GonzoGhanem <ghanemgo@gmail.com>
2013-04-18 18:23:18 -0300

    Fixing some styling in datatables

--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -7,5 +7,8 @@ $(document).ready(function() {
     //bServerSide: true,
     //sAjaxSource: $('#products').data('source')
   });
+  $.extend( $.fn.dataTableExt.oStdClasses, {
+    "sWrapper": "dataTables_wrapper form-inline"
+});
 
 });
