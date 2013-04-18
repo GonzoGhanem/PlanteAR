6828f0076c7639df2c4e3ea7f559ad03d0f0115c
GonzoGhanem <ghanemgo@gmail.com>
2013-04-18 17:58:50 -0300

    Added basic datatable to products index page. Trello #9

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,6 @@
 //= require jquery
-//= require jquery_ujs
+//= require dataTables/jquery.dataTables
+//= require dataTables/jquery.dataTables.bootstrap
 //= require bootstrap
 //= require_tree .
 $(document).ready(function() {
--- app/assets/javascripts/products.js
+++ app/assets/javascripts/products.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
   $('.tooltipField').tooltip();
-  //hover( 
-    //function () {
-      //$(this).tooltip('show');
-    //},
-    //function () {
-      //$(this).tooltip('hide');
-      //}
-  // );    
+  $('#products').dataTable({
+    "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+  	"sPaginationType": "bootstrap"
+    //bProcessing: true,
+    //bServerSide: true,
+    //sAjaxSource: $('#products').data('source')
+  });
+
 });
