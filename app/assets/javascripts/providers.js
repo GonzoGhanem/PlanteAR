e261b90fb024b0e794a13cb861ffb3ce930de7d2
GonzoGhanem <ghanemgo@gmail.com>
2013-04-21 20:02:10 -0300

    Trello_14

---
+++ app/assets/javascripts/providers.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#providers').dataTable({
+    "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
+  	"sPaginationType": "bootstrap"
+    //bProcessing: true,
+    //bServerSide: true,
+    //sAjaxSource: $('#products').data('source')
+  });
+  $.extend( $.fn.dataTableExt.oStdClasses, {
+    "sWrapper": "dataTables_wrapper form-inline"
+});
+
+});
